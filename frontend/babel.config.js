bc0cce47acd623509df065ef4b5c4786da4e39ed
Manoj Kumar <manojkumarthasubilli@gmail.com>
2026-01-04 16:00:50 +0530

    updated dependancies

---
+++ frontend/babel.config.js
@@ -0,0 +1,9 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin', // This now internally includes worklets support in v4
+    ],
+  };
+};
